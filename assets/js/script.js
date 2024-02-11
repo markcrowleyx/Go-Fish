f83718c6434f3235f6edc0dd774f8dbc812faf7e
markcrowleyx <markcrowleyx@gmail.com>
2024-02-11 14:34:39 +0000

    Create allFish array containing 8 images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 let allFish = [
-    []
-]
+    "images/albacore-tuna.png",
+    "images/bass.png",
+    "images/blue-shark.png",
+    "images/bluefin-tuna.png",
+    "images/bream.png",
+    "images/brill.png",
+    "images/coalfish.png",
+    "images/cod.png"
+];
