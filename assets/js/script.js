75b1d5555c419b616427bbb9f28233b12ec370c2
markcrowleyx <markcrowleyx@gmail.com>
2024-02-11 17:12:47 +0000

    Add all 32 fish images to allFish array

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,34 @@ let allFish = [
     "/assets/images/bream.png",
     "/assets/images/brill.png",
     "/assets/images/coalfish.png",
-    "/assets/images/cod.png"
+    "/assets/images/cod.png",
+    "/assets/images/conger-eel.png",
+    "/assets/images/dogfish.png",
+    "/assets/images/flounder.png",
+    "/assets/images/garfish.png",
+    "/assets/images/haddock.png",
+    "/assets/images/hake.png",
+    "/assets/images/herring.png",
+    "/assets/images/john-dory.png",
+    "/assets/images/ling.png",
+    "/assets/images/mackerel.png",
+    "/assets/images/monkfish.png",
+    "/assets/images/mullet.png",
+    "/assets/images/octopus.png",
+    "/assets/images/plaice.png",
+    "/assets/images/pollock.png",
+    "/assets/images/porbeagle-shark.png",
+    "/assets/images/red-gurnard.png",
+    "/assets/images/red-mullet.png",
+    "/assets/images/skate.png",
+    "/assets/images/sprat.png",
+    "/assets/images/squid.png",
+    "/assets/images/thornback-ray.png",
+    "/assets/images/turbot.png",
+    "/assets/images/wrasse.png",
 ];
+
+
 
 // Display images from allFish array in fish-pool area
 function dealCards() {
