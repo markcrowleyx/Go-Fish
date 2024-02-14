f4f70a12ed71e0b4899c6550d48feba7b963b141
markcrowleyx <markcrowleyx@gmail.com>
2024-02-14 17:33:48 +0000

    Add name and alt properties to each image in the allFish array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,36 @@
 let allFish = [
-    "assets/images/albacore-tuna.png",
-    "assets/images/bass.png",
-    "assets/images/blue-shark.png",
-    "assets/images/bluefin-tuna.png",
-    "assets/images/bream.png",
-    "assets/images/brill.png",
-    "assets/images/coalfish.png",
-    "assets/images/cod.png",
-    "assets/images/conger-eel.png",
-    "assets/images/dogfish.png",
-    "assets/images/flounder.png",
-    "assets/images/garfish.png",
-    "assets/images/haddock.png",
-    "assets/images/hake.png",
-    "assets/images/herring.png",
-    "assets/images/john-dory.png",
-    "assets/images/ling.png",
-    "assets/images/mackerel.png",
-    "assets/images/monkfish.png",
-    "assets/images/mullet.png",
-    "assets/images/octopus.png",
-    "assets/images/plaice.png",
-    "assets/images/pollock.png",
-    "assets/images/porbeagle-shark.png",
-    "assets/images/red-gurnard.png",
-    "assets/images/red-mullet.png",
-    "assets/images/skate.png",
-    "assets/images/sprat.png",
-    "assets/images/squid.png",
-    "assets/images/thornback-ray.png",
-    "assets/images/turbot.png",
-    "assets/images/wrasse.png",
+    {"image": "assets/images/albacore-tuna.png", "name": "Alabcore-Tuna", "alt": "Albacore-Tuna"},
+    { "image": "assets/images/bass.png", "name": "Bass", "alt": "Bass" },
+    { "image": "assets/images/blue-shark.png", "name": "Blue-Shark", "alt": "Blue-Shark" },
+    { "image": "assets/images/bluefin-Tuna.png", "name": "Bluefin-Tuna", "alt": "Bluefin-Tuna" },
+    { "image": "assets/images/bream.png", "name": "Bream", "alt": "Bream" },
+    { "image": "assets/images/brill.png", "name": "Brill", "alt": "Brill" },
+    { "image": "assets/images/coalfish.png", "name": "Coalfish", "alt": "Coalfish" },
+    { "image": "assets/images/cod.png", "name": "Cod", "alt": "Cod" },
+    { "image": "assets/images/conger-eel.png", "name": "Conger-Eel", "alt": "Conger-Eel" },
+    { "image": "assets/images/dogfish.png", "name": "Dogfish", "alt": "Dogfish" },
+    { "image": "assets/images/flounder.png", "name": "Flounder", "alt": "Flounder" },
+    { "image": "assets/images/garfish.png", "name": "Garfish", "alt": "Garfish" },
+    { "image": "assets/images/haddock.png", "name": "Haddock", "alt": "Haddock" },
+    { "image": "assets/images/hake.png", "name": "Hake", "alt": "Hake" },
+    { "image": "assets/images/herring.png", "name": "Herring", "alt": "Herring" },
+    { "image": "assets/images/john-dory.png", "name": "John-Dory", "alt": "John-Dory" },
+    { "image": "assets/images/ling.png", "name": "Ling", "alt": "Ling" },
+    { "image": "assets/images/mackerel.png", "name": "Mackerel", "alt": "Mackerel" },
+    { "image": "assets/images/monkfish.png", "name": "Monkfish", "alt": "Monkfish" },
+    { "image": "assets/images/mullet.png", "name": "Mullet", "alt": "Mullet" },
+    { "image": "assets/images/octopus.png", "name": "Octopus", "alt": "Octopus" },
+    { "image": "assets/images/plaice.png", "name": "Plaice", "alt": "Plaice" },
+    { "image": "assets/images/pollock.png", "name": "Pollock", "alt": "Pollock" },
+    { "image": "assets/images/porbeagle-shark.png", "name": "Porbeagle-Shark", "alt": "Porbeagle-Shark" },
+    { "image": "assets/images/red-gurnard.png", "name": "Red-Gurnard", "alt": "Red-Gurnard" },
+    { "image": "assets/images/red-mullet.png", "name": "Red-Mullet", "alt": "Red-Mullet" },
+    { "image": "assets/images/skate.png", "name": "Skate", "alt": "Skate" },
+    { "image": "assets/images/sprat.png", "name": "Sprat", "alt": "Sprat" },
+    { "image": "assets/images/squid.png", "name": "Squid", "alt": "Squid" },
+    { "image": "assets/images/thornback-ray.png", "name": "Thornback-Ray", "alt": "Thornback-Ray" },
+    { "image": "assets/images/turbot.png", "name": "Turbot", "alt": "Turbot" },
+    { "image": "assets/images/wrasse.png", "name": "Wrasse", "alt": "Wrasse" }
 ];
 
 //Select 8 random images from allFish.
@@ -49,11 +49,12 @@ function dealCards() {
         fishCardElement.classList.add("fish-card");
         fishCardElement.innerHTML = `        
         <div class= "front">
-        <img class = "fish-image" src="${fish}"/>
+        <img class = "fish-image" src="${fish.image}" alt="${fish.alt}"/>
         </div>
         <div class="back"></div>
         `;
         fishPool.appendChild(fishCardElement);
+        fishCardElement.addEventListener('click', function () { console.log(this) });
 
     }
 }
